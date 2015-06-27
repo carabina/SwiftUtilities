bb14475f8c0502f5f7b62bf237c9ce82d37b685d
Jonathan Wight <jwight@mac.com>
2015-06-27 06:50:21 -0700

    Random example.

--- Playgrounds/Random.playground/Contents.swift
+++ Playgrounds/Random.playground/Contents.swift
@@ -4,3 +4,18 @@ import Cocoa
 
 import SwiftUtilities
 
+let suits = ["♥", "♣", "♦", "♠"]
+
+let values = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "J", "Q", "K"]
+
+var deck = suits.flatMap() {
+    (suit:String) in
+    return values.map() {
+        (value:String) in
+        return value + suit
+    }
+}
+
+deck = random.shuffled(deck)
+
+(deck[0], deck[1])
