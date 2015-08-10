6e2a90ce71ea9d0d630492e4a302a2e53bce88b4
Jonathan Wight <jwight@mac.com>
2015-08-10 11:58:00 -0700

    Added bitwiseEquality

---
+++ SwiftUtilities/Utilities.swift
@@ -0,0 +1,17 @@
+//
+//  Utilities.swift
+//  SwiftUtilities
+//
+//  Created by Jonathan Wight on 8/10/15.
+//  Copyright © 2015 schwa.io. All rights reserved.
+//
+
+import Foundation
+
+public func bitwiseEquality <T> (lhs:T, _ rhs:T) -> Bool {
+    var lhs = lhs
+    var rhs = rhs
+    return withUnsafePointers(&lhs, &rhs) {
+        return memcmp($0, $1, sizeof(T))  == 0
+    }
+}
