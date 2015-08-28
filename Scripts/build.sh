3d39c432354109088dbe7f63b43c1adb550fb624
Jonathan Wight <jwight@mac.com>
2015-08-28 08:53:27 -0700

    Updated for Travis.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -6,4 +6,5 @@ xcodebuild -version | grep "Xcode 7" > /dev/null || { echo 'Not running Xcode 7'
 
 cd `git rev-parse --show-toplevel`
 
-xctool -project SwiftUtilities.xcodeproj -scheme All build test || exit $!
+xctool -project SwiftUtilities.xcodeproj -scheme "SwiftUtilities_OSX" -sdk macosx build test || exit $!
+xctool -project SwiftUtilities.xcodeproj -scheme "SwiftUtilities_iOS" -sdk iphonesimulator build test || exit $!
